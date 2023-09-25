1f6ffa194a99d89cb39f2605c89faf9def34491d
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2023-09-25 15:49:50 +0000

    Datatypes complete

--- JS_Basic/datatypes_02.js
+++ JS_Basic/datatypes_02.js
@@ -1,2 +1,20 @@
 "use strict"; //this is used for old code treat as new ver code
 // alert(5"or any arguments") here it can't be run because we use node js not browser this runs in browser where JS engine is present
+let name = "Jarvis"  // string datatype
+let age = 24            //number datatype
+let isLoggedIn = false   //bolean dataype
+
+// premative Data Types
+// Number datatype range up to 2 to power 53
+// bigent datatype bigger than Number datatype
+// string use ""
+// boolean = true/ false
+// null = nothing even not 0
+// undefined = 
+// symbol
+
+// Objects
+console.log(typeof "jarvis")
+// Null is an Object datatype
+// if we check data type of null it shows an Object
+console.log(typeof null)
