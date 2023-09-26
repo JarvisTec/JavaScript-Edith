9bf7f339b8b959cb5bf8f5fa3e4cd36f609f962f
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2023-09-26 16:55:59 +0000

    Operators complete

---
+++ JS_Basic/Operators_03.js
@@ -0,0 +1,32 @@
+// Operations
+let a=3
+console.log(a)
+// but if we want to display value of var a in -ive then
+let b=-a
+console.log(b)  //this shows -ive value
+
+// Operators are {these are methimitacal opetrators i put these as comment to avoid for show in result but if we remove double // slash it runs}
+// console.log(2+2)  //addition
+// console.log(2-2)    //subtraction
+// console.log(2*2)    //multiplication
+// console.log(2**2)   //Power
+// console.log(2/2)    //divsion
+// console.log(2%2)    //ger reminder
+
+// concatenate and + addition OPerator
+let str1 = "4"
+let str2 = "4"
+let str3 = str1+str2
+console.log(str3)   //output is 44 because here + performance concatnation not addition 
+
+// number + string = string
+// string + string = string
+ console.log("1"+2)     //string + number = string output 12
+ console.log("1"+"2")   // string + string = string output 12
+ console.log(1+"2")     // number + string = string output 12
+ console.log("1"+2+2)   //string + number + number = string output 122
+ console.log(1+1+"2")   // number + number + string = ist oprands get add and then concatinate with string output 22
+
+//  best way to write a code to perform many arthimitcical operations at a time i.e
+console.log((3 + 3) * 5 )
+
