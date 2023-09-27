514dcf3bc440511b7ba79cfd8aab1eab3b01276d
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2023-09-27 15:25:42 +0000

    Conversion and Operators done

---
+++ JS_Basic/comprasion_4.js
@@ -0,0 +1,16 @@
+// comprasion operators are 
+// < lessertha
+// > greaterthan
+// <=   lesseror equal
+// >=   greater or equal
+// =    comprasion
+// ==   assgin any value
+// !=   not equals
+
+console.log("2">1);
+console.log("02">1)
+
+// now what abt null
+console.log(null < 0)       //false here null treats as numbe 0
+console.log(null == 0)      //false 
+console.log(null >= 0)      //true
