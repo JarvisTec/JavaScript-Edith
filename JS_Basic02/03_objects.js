3346298edc49bda9b08e2ee7ca22dc102f1852f1
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2023-10-02 05:10:30 +0000

    Complete Objects part ist

--- JS_Basic02/03_objects.js
+++ JS_Basic02/03_objects.js
@@ -20,21 +20,40 @@ const user1 =
     logedin : "false",
     logindays : ["monday", "Tuesday"]
 }
-console.log(user1.email);
-console.log(user1["email"]); //this is an another way to access the elements of an object without ". dot" operator
+// console.log(user1.email);
+// console.log(user1["email"]); 
+//this is an another way to access the elements of an object without ". dot" operator
 // Reason why we use without ". dot" operator because if any element of an Object is in string form then their we can't use ". dot" operator like "Status" element of User1 object.
 
-console.log(user1["status"]);
-console.log(user1[mySym]);  //here we display the value fo symbol key yes it displays easyly by it does not treats as symbol data type it acts as string data type. to treats as symbol data type we need to write in [] brackets like [mySym]
+// console.log(user1["status"]);
+// console.log(user1[mySym]);  
+//here we display the value fo symbol key yes it displays easyly by it does not treats as symbol data type it acts as string data type. to treats as symbol data type we need to write in [] brackets like [mySym]
 
 
 //Replace value of keys eg. change email
-user1.email = "jarvisnet@gmail.com"
-console.log(user1["email"])   
-//to freez an object. means after freez we can't change any value of any key
-Object.freeze(user1);
-user1.email = "edithnet@gmail.com";  //email can't be changed because freez()
-console.log(user1)  
+// user1.email = "jarvisnet@gmail.com"
+// console.log(user1["email"])   
+// //to freez an object. means after freez we can't change any value of any key
+// // Object.freeze(user1);
+// user1.email = "edithnet@gmail.com";  //email can't be changed because freez()
+// console.log(user1)  
+
+//Add function inObject as key/ element of an Object. and these functions are treat as simple varible.
+
+user1.greeting = function()
+{
+    console.log("Hello Jarvis");
+}
+console.log(user1.greeting());    
+//before run this remove or diable codeline which freezs object. because we know that if we freez object then we can't add any function inan object.
+console.log(user1.greeting);  //if we give ".greating" without () parenthises it returns "[Function (anonymous)]" 
 
 
-
+//another functtion here we are refrencing one of property of an ibject "name" for this we use "string interpolation" with backticks.
+user1.greetingTwo = function()
+{
+    console.log(`Hello Jarvis, ${this.name} `);
+}
+console.log(user1.greetingTwo());    
+//The difference is that inprevious we display only value but here we are also refrence with property of an Onject with "this" keyword.
+//and if presently undefined is shown in result jus ignore this time
