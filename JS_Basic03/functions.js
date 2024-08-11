7b00027cd80059a9126dc0f8e494d11510480aa3
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2024-08-11 14:28:43 +0000

    Passing number of values at a time as an argument and using Objects & Arrays in an Object

--- JS_Basic03/functions.js
+++ JS_Basic03/functions.js
@@ -98,13 +98,66 @@ console.log(lgnmsg());
 // Creat functions to take number of values or pass number of values like in shopping cart where user add number of items having different price ehic is added also
 
 
-function cartitem(item1)
+function cartitem(...item1)
+// function cartitem(val1, val2, ...item1)         
 {
     return item1
 }
 
-console.log(item1(2));
+console.log(cartitem(200, 150, 450, 510, 210));    
+
+// in this function we pass one value but user can add number of items in Cart means as arguments number of values may be passed in that case it gives one value only SO in this case we  use "RestOperator" in parameters like as "Sepred Operator" depedent where we use
+//But result gives in "Array []"
+//"function cartitem(val1, val2, ...item1)" by this we get 3 values because ist 2     values are stored in "val1" & "val2" and rest of values stured in "item1"
 
 
+
+
+
+// using Objects in function******************
+
+const obj=
+{
+    name: "jarvis",
+    id: 2591
+}
+
+function handelobj(anyobj)   //hewrw we pass "anyobj" as temp because if we have more Obj
+{
+    console.log(`User's name is ${anyobj.name} and ID is ${anyobj.id}`)
+}
+
+// handelobj(obj)        //ist way to call function
+
+//in this function we are not passing object directly because if we have number of objects so for that we pass a valve in parameters as temp "anyobj" but when we call the function we give the object as argument ie "obj" by this object are passed in parameters and it dedetc the object and display values which we want to display in an function 
+
+// Now if "name" or "ID" is not present or spelling is not matched in an object their we use condition statement
+
+handelobj({
+    name: "Edith",
+    id: 5903
+})        
+//2nd way to call function in this we creat an object withi arguments here we are not passing object as argument by its name but we declare object within argument
+
+
+
+
+
+//function using arrays************************
+
+
+const arr = [25, 38, 96, 75]
+
+function handelarr(anyarr)   //hewrw we pass "anyarr" as temp because if we have more array
+{
+    return anyarr[1]
+}
+
+// console.log(handelarr(arr));
+
+console.log(handelarr([25, 38, 96, 75]));
+
+
+// Same as we can pass arrays by declaring Arrays withing arguments
 
 
