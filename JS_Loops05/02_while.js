913bd6b942fc4665b97d1af1d9c921e48745186d
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2024-09-22 12:47:52 +0000

    Complete While and Do_while Loops

---
+++ JS_Loops05/02_while.js
@@ -0,0 +1,30 @@
+// let i = 0
+// while (i <= 10) 
+// {
+//     console.log(`The value is: ${i}`);
+//     i = i + 2;
+// }
+
+
+
+// loop on array 
+
+let myarr = ["Iroman", "Thor", "Captain", "Spiderman", "Hulk"]
+
+let indx = 0
+while (indx < myarr.length) 
+{
+    console.log(`The values is ${myarr[indx]}`);
+    indx = indx + 1
+}
+
+
+
+
+// Do loop: in this case code is onece excuted before checking the condition
+
+num = 1
+do {
+    console.log(`the value is ${num}`);
+    num++    
+} while (num <= 10);
