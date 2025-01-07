68b0ba126a1a2d0cbf450be3ec6340772c0ad7c7
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2025-01-07 13:05:17 +0000

    Half Filter

--- JS_Loops05/04_foreach_loop.js
+++ JS_Loops05/04_foreach_loop.js
@@ -69,3 +69,19 @@ const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const newnum = num.filter( (items) => {return items%2 == 0})  //Explicit
 console.log(newnum);
+
+
+
+//Use filter on vast data eg
+
+const class1 = [
+  {name: " Student1", marks: "445", Grade: "A"},
+  {name: " Student2", marks: "325", Grade: "A"},
+  {name: " Student3", marks: "375", Grade: "B"},
+  {name: " Student4", marks: "425", Grade: "A"},
+  {name: " Student5", marks: "355", Grade: "b"},
+  {name: " Student6", marks: "225", Grade: "C"},
+  {name: " Student7", marks: "285", Grade: "C"},
+]
+
+const std = class1.filter( (value) => )
