8561fe15a29cc9e6493d7e57a46e2fc36c79daba
JarvisTec <42837803+JarvisTec@users.noreply.github.com>
2025-01-07 06:11:30 +0000

    Extra For_each loop and Filter

--- JS_Loops05/04_foreach_loop.js
+++ JS_Loops05/04_foreach_loop.js
@@ -39,6 +39,33 @@ const myarr =[
 ]
 
 myarr.forEach( function(item){
-  console.log(item.name);
+  // console.log(item.name);
   
 })
+
+
+// for each loop can return value because there we use function so let us se
+
+const avangers = ["stark", "Captain", "petter", "Thor"]
+//here we store for_each loop in varible to check what it returns but in actual it never returns any value
+
+const val = avangers.forEach( (items) => {
+  // console.log(items);
+  return items;        
+} )
+// console.log(val);
+
+
+// Filter: The filter() method creates a new array containing elements that satisfy a specified condition. This method skips empty elements according to condition and does not change the original array.
+// Create a new array consisting of only those elements that satisfy the condition
+// so here we use condition but in for_each loop we take all items of given array.
+
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const newnum = num.filter( (items) => items%2 == 0)  //Implicit
+// console.log(newnum);
+
+// Implecit: automatic return a value without {} braces 
+
+const newnum = num.filter( (items) => {return items%2 == 0})  //Explicit
+console.log(newnum);
